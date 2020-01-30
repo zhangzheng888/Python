71e8f365c5f4d7ca8fb5987895212fcf8a03435a
Kevin Zhang <k_zhang@wargaming.net>
2020-01-30 00:14:53 -0800

    Optimized with PEP8 and Flake8

--- Strings/yourPurpose.py
+++ Strings/yourPurpose.py
@@ -1,10 +1,10 @@
 # This first python program says hello and asks for my age
 
-print ('Hello buddies')
-print ('What is your purpose?') # asks a deep life-long thinking question
+print('Hello buddies')
+print('What is your purpose?')  # asks a deep life-long thinking question
 yourPurpose = input()
-print ('That is your purpose for right now')
-print ('The length of your purpose is: ' + yourPurpose)
-print ('What is your age?')
+print('That is your purpose for right now')
+print('The length of your purpose is: ' + yourPurpose)
+print('What is your age?')
 yourAge = input()
-print ('It will take ' + yourAge + ' before your purpose will be realized,')
+print('It will take ' + yourAge + ' before your purpose will be realized,')
