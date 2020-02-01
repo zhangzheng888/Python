7bde3e292de8dc6532d3b5375936fdb07adb9361
Kevin Zhang <k_zhang@wargaming.net>
2020-02-01 00:00:33 -0800

    Update numbers python file with docstring for each number data type

--- Python 2/01. Data Types/001 Numbers.py
+++ Python 2/01. Data Types/001 Numbers.py
@@ -9,12 +9,15 @@ number = 8
 
 # Output: <class 'int'>
 print(type(number))
+# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 
 # Data Type Float
-number = 8.0
+number = -8.0
 
 # Output: <class 'float'>
 print(type(number))
+# Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
 
 # Data Type Complex
 complex_number = 5 + 3j
@@ -24,3 +27,4 @@ print(complex_number + 3)
 
 # Output: True
 print(isinstance(complex_number, complex))
+# Complex numbers are written with a "j" as the imaginary part.
