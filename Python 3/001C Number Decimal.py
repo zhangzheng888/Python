a246b04932c888eb7c93ccd6f346a3a185335c40
Kevin Zhang <k_zhang@wargaming.net>
2020-04-10 23:54:33 -0700

    Initial Python 3 Decimals example script

---
+++ Python 3/001C Number Decimal.py
@@ -0,0 +1,14 @@
+import decimal
+
+# Output false
+(1.1 + 2.2) == 3.3
+
+# Proof
+1.1 + 2.2
+# Output 3.3000000000000003
+
+# Output: 0.1
+print(0.1)
+
+# Output: Decimal('0.1000000000000000055511151231257827021181583404541015625')
+print(decimal.Decimal(0.1))
