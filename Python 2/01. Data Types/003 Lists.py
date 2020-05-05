10cf9dc581d28a438ff7d4d289eae85d62b2fd1f
Kevin Zhang <k_zhang@wargaming.net>
2020-05-04 23:34:49 -0700

    Create Python 2.7 Lists Data Type example script

---
+++ Python 2/01. Data Types/003 Lists.py
@@ -0,0 +1,22 @@
+"""
+Data Type: Lists (Python 2.7)
+
+In Python programming, a list is created by placing all the items (elements) inside a square
+brackets [], separated by commas.
+
+List is one of the most frequently used and very versatile data types used in Python.
+
+A list can have any number of items and they may be of different types (integer, float, string etc.).
+"""
+
+# empty list
+list_example_empty = []
+
+# list of integers
+list_example_int = [1, 2, 3]
+
+# list with mixed data types
+list_example_mixed = [1, "Hello", 3.4]
+
+# nested list
+list_example_nested = ["mouse", [8, 4, 6], ['a']]
