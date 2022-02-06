562066494d2c44b951dd11b4d14c6d4546a640aa
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-05 23:55:50 -0800

    Create linear_search.py
    Create Python 3 linear search algorithm example

---
+++ Algorithms/linear_search.py
@@ -0,0 +1,15 @@
+"""
+Python Algorithm for Linear Search
+"""
+
+def linear_search(self, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return none
+
+def verify(index):
+    if index is not none:
+        return print("Target is in index ", index)
+    else:
+        return print("Target is not found in list")
