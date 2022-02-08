59286e154a80c150b4fa260b8e48ab341712fa2d
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-08 01:48:09 -0800

    Update Python 3 Linear Search script with docstring comments

--- Algorithms/linear_search.py
+++ Algorithms/linear_search.py
@@ -3,13 +3,32 @@ Python Algorithm for Linear Search
 """
 
 def linear_search(self, target):
+    """
+    Returns the index position of the target if found, else returns None
+    """
     for i in range(0, len(list)):
         if list[i] == target:
             return i
-    return none
+    return None
 
 def verify(index):
-    if index is not none:
+    """
+    Verifies whether the target was found in the target from linear search
+    algorithm
+    """
+    if index is not None:
         return print("Target is in index ", index)
     else:
         return print("Target is not found in list")
+
+"""
+The time complexity of the linear search algorithm is O(n).
+
+Linear search is rarely used practically because other search algorithms such
+as the binary search algorithm and hash tables allow significantly
+faster-searching comparison to Linear search.
+
+Improve Linear Search Worst-Case Complexity
+
+- if element found on the first try O(n) to O(1)
+"""
