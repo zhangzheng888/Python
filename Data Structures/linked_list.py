b5571f02b1b34051b8de6ac09306ce819911d33c
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-14 23:46:51 -0800

    Update Python 3 Linked List script with add, search, insert and remove methods

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -34,6 +34,78 @@ class LinkedList:
             current = current.next_node
         return count
 
+    def add(self, data):
+        """
+        Adds new Node containing data at the head of the list
+        Takes O(1) time
+        """
+
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        """
+        current = self.Head
+
+        if current:
+            if current.data = key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the node at the
+        insertion point takes O(n) time.
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new_node = Node(data)
+
+            position = index
+            current = self.Head
+
+            while position > 1:
+                current = new_node.next_node
+                position -= 1
+
+            previous_node = current
+            next_node = current.next_node
+
+            previous_node.next_node = new_node
+            new_node.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data matching the key
+        Returns the node or None if key doesn't exist
+        Takes O(n) time
+        """
+        current = self.Head
+        previous_node = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+
+            elif current.data == key:
+                found = True
+                previous_node.next_node = current.next_node
+
+            else:
+                previous_node = current
+                current = current.next_node
+        return current
+
     def __repr__(self):
         """
         Return a string representation of the list
@@ -50,3 +122,7 @@ class LinkedList:
                 nodes.append(f"[Tail: {current.data}]")
             else:
                 nodes.append(f"{current.data}")
+
+            current = current.next_node
+
+        return "-> ".join(nodes)
