b4d1f3e16eba865b5f6d7d250706c245e4de0afc
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-17 23:26:18 -0800

    Create Python 3 Sets Data Structure script

---
+++ Data Structures/sets.py
@@ -0,0 +1,67 @@
+"""
+Sets
+
+- Store non-duplicate items
+- Very fact access vs. lists (hash vs looping)
+- Math Set operations (union, intersect)
+- Sets are unordered and can't be sorted
+
+"""
+
+"""
+Constructors
+"""
+
+set_constructor_type_1 = {3, 5, 3, 5}
+
+set_constructor_type_2 = {}
+
+list_sample = [1, 2, 4]
+
+set_constructor_from_list = set(list_sample)
+
+"""
+Operations
+
+Add
+set.add
+
+Remove
+set.remove
+
+Check length
+len(set)
+
+Check membership
+print(5 in set)
+
+Pop random item from set
+set.pop()
+
+Delete all items from set
+set.clear()
+"""
+
+"""
+Mathematical Set Operations
+
+Intersection (AND)
+
+(set1 & set2)
+
+Union (OR)
+
+(set1 | set2)
+
+Symmetric Difference (XOR)
+Difference item in set1 but NOT set2
+
+set1 ^ set2
+
+Subset (set2 contains set1)
+set1 <= set2
+
+Superset (set1 contains set2)
+set1 >= set2
+
+"""
