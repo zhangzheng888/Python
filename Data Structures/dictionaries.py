b6ee6e0722e515c9da260dbb499da53a385f1e15
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-18 23:27:04 -0800

    Create Python 3 Dictionaries Data Structure script

---
+++ Data Structures/dictionaries.py
@@ -0,0 +1,71 @@
+"""
+Dictionaries (dict)
+
+- Key/value pairs
+- Associative array, like Java HashMap
+- Unordered
+
+"""
+
+
+"""
+Constructors
+
+
+dict_sample_1 = {'pork': 35, 'chicken': 22.7}
+
+dict_sample_2 = {[('pork', 25.3), ('chicken', 22.7)]}
+
+dict_sample_3 = (pork=20, beef=30)
+"""
+
+"""
+Operations
+
+Add/Update
+
+dict_sample_1['shrimp'] = 10
+
+Delete
+
+del(dict_sample_1['shrimp'])
+
+Lengh
+
+len(dict_sample_1)
+
+Delete All
+
+dict_sample_1.clear()
+
+Delete Dict
+
+del(dict_sample_1)
+
+
+"""
+
+
+"""
+Accessing Key/Values
+* Python 3 only
+
+dict_sample_1.keys()
+dict_sample_2.values()
+dict_sample_3.items() key/value pairs
+
+
+Check membership in keys *only looks in keys
+if 'beef' in dict_sample_1
+
+Check membership in values
+if 'clams' in dict_sample_1.values()
+"""
+
+"""
+Iterating Dictionaries
+
+for key in dict_sample_1:
+
+for key, value in dict_sample_2.items()
+"""
