83b1783b5d09e3b365efa69e3b7fdb52fa82d9ba
Kevin Zhang <zhangzheng888@gmail.com>
2022-02-21 23:37:01 -0800

    Create Python 3 Queues Data Structure script

---
+++ Data Structures/queues.py
@@ -0,0 +1,32 @@
+"""
+Queues
+
+FIFO (first-in, first-out)
+
+Enqueue - add an item to the end of the line
+Dequeue - remove an item from the front of the line
+
+
+"""
+
+
+"""
+Queues using Python Deque
+
+Deque is a double-ended queue
+
+Append() to enqueue an item
+Popleft() to dequeue an item
+
+
+from collections import deque
+
+queue = deque()
+
+queue.append(1)
+
+queue.popleft()
+
+
+
+"""
